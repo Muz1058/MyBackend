d0aa82f6f3b9893b8e7ec524b3fb9401da88bee1
Muz1058 <muzaffarali3633@gmail.com>
2025-08-03 10:12:51 -0700

    writting controllers loGIC

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,29 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
+import {ApiError} from '../utils/ApiError.js'
 
 const registerUser= asyncHandler( async (req,res)=>{
-    res.status(200).json(
-        {
-            message:"ok"
-        }
-    )
+    //get user details from frontend
+    //validation not-empty
+    //check user alredy exist (by email or username)
+    //check user files (avater,cover image) ,
+    //upload them to cloudinary (avater)
+    //cretae  user object  - create entry in db
+    // remove password and refresh token field from response
+    // check user cteation response
+    //return response if avilable
+
+    const {fullname,email,username,password} =req.body;
+
+    console.log("email ::", email);
+    console.log("password ::", password);
+
+    if([fullname,username,password,email].some((field)=>field?.trim()==="")
+    ){
+        throw new ApiError(400,"All fields are compulsory")
+    }
+    
+
+    
 
 })
 
