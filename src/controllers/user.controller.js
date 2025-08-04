d425a395527864fe52ca92531fe2cd841e3f8893
Muz1058 <muzaffarali3633@gmail.com>
2025-08-04 10:26:32 -0700

    Add register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,8 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
-import {ApiError} from '../utils/ApiError.js'
+import {ApiError} from '../utils/ApiError.js';
+import {User} from '../models/user.model.js'
+import { uploadOnCloudinary } from '../utils/cloudinary.js';
+import { ApiResponse } from '../utils/ApiResponse.js';
 
 const registerUser= asyncHandler( async (req,res)=>{
     //get user details from frontend
@@ -21,10 +24,57 @@ const registerUser= asyncHandler( async (req,res)=>{
     ){
         throw new ApiError(400,"All fields are compulsory")
     }
-    
+
+   const existedUser=  User.findOne({
+        $or:[{username},  {email}]
+    })
+
+    if(existedUser){
+        throw new ApiError(409, "user with username or email already exist")
+    }
 
     
 
+    const avatarLocalPath= req.files?.avatar[0]?.path;
+    const coverImageLocalPath= req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError (400,"Avatar file is required")
+    }
+
+    const avatar= await uploadOnCloudinary(avatarLocalPath);
+    if(coverImageLocalPath){
+        const coverImage = await uploadOnCloudinary(coverImageLocalPath); 
+    }
+
+    if(!avatar){
+        throw new ApiError (400,"Avatar file is required to upload on server")
+    }
+    
+    const user= await User.create({
+        fullname,
+        username :username.toLowerCase(),
+        email,
+        avatar:avatar.url,
+        coverImage:coverImage?.url|| "",
+        password
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500,"Something went wrong while user registration")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"User registerd sussessfully")
+    )
+
+
+    console("req.body ::",req.body);
+    console("req.files ::",req.files)
 })
 
 
